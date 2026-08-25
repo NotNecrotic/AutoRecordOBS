5d8ddb795b22a4ba60e99cf0bd36f52b7794cf1b
Necrotic <fearndevon@gmail.com>
2026-08-25 17:52:46 +1200

    Add executable metadata and fix package version

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
+        
+        res.set("ProductName", "AutoRecordOBS");
+        res.set("FileDescription", "AutoRecordOBS Tray Application");
+        res.set("OriginalFilename", "AutoRecordOBS.exe");
+
         res.set_icon("assets/icon.ico");
+
         res.compile().unwrap();
     }
 }
